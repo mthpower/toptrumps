958f96d0cd3b9cdc77971eecef16cd1c44acdff5
Matthew Power <matthew@mthpower.uk>
2017-05-04 21:13:16 +0100

    MY stuff

--- toptrumps.py
+++ toptrumps.py
@@ -1,6 +1,8 @@
 import json
 import asyncio
 import aiohttp
+import operator
+from collections import Counter
 
 def url(id):
     return f'https://candidates.democracyclub.org.uk/api/v0.9/persons/{id}/'
@@ -8,7 +10,7 @@ def url(id):
 async def main():
     tasks = []
     async with aiohttp.ClientSession() as session:
-        for i in range(1, 2):
+        for i in range(1, 200):
             tasks.append(get_candidate_json_by_id(session, i))
 
         return await asyncio.gather(*tasks)
@@ -21,12 +23,19 @@ async def get_candidate_json_by_id(session, id):
 
 
 def extract_card(person):
-    name = person['name']
-    gender = person['gender']
-    email = person['email']
-    party_memberships = person['party_memberships']
-    latest = max([int(k) for k in party_memberships.keys()])
-    print(latest)
+    name = person.get('name')
+    gender = person.get('gender')
+    email = person.get('email')
+    try:
+        party_memberships = person['versions'][0]['data']['party_memberships'].values()
+        parties = Counter(m['name'] for m in party_memberships)
+        party = sorted(parties.items(), key=operator.itemgetter(1))[0][0]
+    except KeyError:
+        party = None
+
+    return (name, gender, email, party)
+
+def create_table(card):
 
 
 def score_email_address(email_address):
